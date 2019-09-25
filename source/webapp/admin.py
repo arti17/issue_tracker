a2ad7977cd45b83986033a8304022a33e1e47800
Artem Tkachuk <arti17900@gmail.com>
2019-09-25 22:48:21 +0600

    Создание моделей задачи, статуса и типа. Добвление в админ панель

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,2 +1,13 @@
 from django.contrib import admin
+from .models import Issue, Status, Type
 
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['summary', 'status', 'type', 'create_date']
+    list_filter = ['status', 'type']
+    search_fields = ['summary']
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,1 +1,38 @@
 from django.db import models
+
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=60, verbose_name='Краткое описание')
+    description = models.TextField(max_length=400, null=True, blank=True, verbose_name='Полное описание')
+    status = models.ForeignKey('webapp.Status', related_name='issues', on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', related_name='issues', on_delete=models.PROTECT, verbose_name='Тип')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.summary
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Статус задачи'
+        verbose_name_plural = 'Статусы задач'
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Тип')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тип задачи'
+        verbose_name_plural = 'Типы задач'
