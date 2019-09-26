6587c6926c6633a0357075087e23c82537f6174e
Artem Tkachuk <arti17900@gmail.com>
2019-09-26 11:25:03 +0600

    Добавление CRUD для задач

---
+++ source/webapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import widgets
+from webapp.models import Issue, Status, Type
+
+
+class IssueForm(forms.Form):
+    summary = forms.CharField(max_length=60, required=True, label='Краткое описание')
+    description = forms.CharField(max_length=400, required=False, label='Полное описание', widget=widgets.Textarea)
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус задачи',
+                                    empty_label='Выберите статус задачи')
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label='Тип задачи',
+                                  empty_label='Выберите тип задачи')
--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('issue/<int:pk>', views.IssueView.as_view(), name='detail'),
+    path('create_issue/', views.IssueCreateView.as_view(), name='create_issue'),
+    path('update_issue/<int:pk>', views.IssueUpdateView.as_view(), name='update_issue'),
+    path('delete_issue/<int:pk>', views.IssueDeleteView.as_view(), name='delete_issue'),
 ]
