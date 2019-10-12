ccf20a37546363a2cad23993146e7ccf5017faf2
Artem Tkachuk <arti17900@gmail.com>
2019-10-12 23:01:21 +0600

    Добавлен поиск по названию для проектов.

--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
-from webapp.forms import ProjectForm, IssueProjectForm
+from webapp.forms import ProjectForm, IssueProjectForm, SimpleSearchForm
 from webapp.models import Project, PROJECT_STATUS_BLOCKED, PROJECT_STATUS_ACTIVE
 
 
@@ -10,11 +10,29 @@ class ProjectView(ListView):
     template_name = 'project/project_list.html'
     model = Project
 
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['active_projects'] = Project.objects.filter(status=PROJECT_STATUS_ACTIVE).order_by('create_date')
-        context['blocked_projects'] = Project.objects.filter(status=PROJECT_STATUS_BLOCKED).order_by('create_date')
+        context['form'] = self.form
+        if self.search_value:
+            context['active_projects'] = Project.objects.filter(summary__icontains=self.search_value, status=PROJECT_STATUS_ACTIVE).order_by('create_date')
+            context['blocked_projects'] = Project.objects.filter(summary__icontains=self.search_value, status=PROJECT_STATUS_BLOCKED).order_by('create_date')
+        else:
+            context['active_projects'] = Project.objects.filter(status=PROJECT_STATUS_ACTIVE).order_by('create_date')
+            context['blocked_projects'] = Project.objects.filter(status=PROJECT_STATUS_BLOCKED).order_by('create_date')
         return context
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class ProjectDetailView(DetailView):
