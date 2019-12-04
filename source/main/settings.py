6260f885bdc802d09c845e7efbe352afcde89a2a
Artem Tkachuk <arti17900@gmail.com>
2019-12-04 10:36:03 +0600

    Добавлено приложение api.

--- source/main/settings.py
+++ source/main/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'webapp.apps.WebappConfig',
     'accounts.apps.AccountsConfig',
+    'api.apps.ApiConfig',
     'widget_tweaks',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
